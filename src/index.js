965d4326cfa6a6fa502124bf409b26481d78d7d2
Ivan-Urban <ivan.urbanovich.00@gmail.com>
2022-02-02 19:58:41 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    console.log(expr);
+    const n = expr.length / 10;
+    const arr10 = [];
+    for (let i = 0; i < n; i++){
+        arr10[i] = '';
+        for (let j = 0; j <= 9; j++) {
+            arr10[i] += expr[i*10 + j];
+        } 
+    }
+    const arrOfChar = arr10.map((el) => {
+        if (el != '**********') {
+            let strOfMorse = [];
+            for (let i = 8; i >= 0; i -= 2) {
+                if (el[i] + el [i+1] == '10') {
+                    strOfMorse.unshift('.');
+                } else {
+                    if (el[i] + el[i+1] == '11'){
+                        strOfMorse.unshift('-');
+                    } else {
+                        break;
+                    }
+                } 
+            }
+            return MORSE_TABLE[strOfMorse.join('')];
+        } else {
+            return ' ';
+        }
+    });
+    return arrOfChar.join('');
 }
 
 module.exports = {
     decode
-}
+};
